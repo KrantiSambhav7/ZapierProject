e1247bf53047166e6a136cbe76d55aac004edb85
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-06-14 08:53:01 +0530

    Transactional Pattern Added

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -10,14 +10,19 @@ app.post("/hooks/catch/:userId/:zapId" , async(req , res) => {
     const body = req.body;
     // We want when the user will hit the hooks endpoint, then the entry must be stored in the DB as well as in a queue 
     // But here we use a Transactional Outbox pattern. 
+    // Any random user cannot hit the webhook directly as we share the passwords with the user. 
+    // When we create a Zap and then make a trigger in it then we will get a Webhook URL which is handled by this endpoint.
+    // This URL will be stored in any external service and then they will hit this URL with the data. 
+    // So when the req comes we will then start to execute this.
+    // Here we will create a ZapRun and then create a ZapRunOutbox entry. Two tables are being created here. Outbox table is used to store the data that will be processed later by a worker or a queue system because it is easy to achieve atomicity in DBs.
     await client.$transaction(async tx => {
-        const run = await tx.zapRun.create({
+        const run = await tx.zapRun.create({ // This is the ZapRun entry that will be created for this webhook call
             data:{
                 zapId: zapId,
                 metadata: body
             }
         })
-        await tx.zapRunOutbox.create({
+        await tx.zapRunOutbox.create({ // This is the outbox entry that will be processed later by a worker or a queue system
             data:{
                 zapRunId: run.id
             }
