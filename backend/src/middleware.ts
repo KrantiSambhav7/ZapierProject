f4bd02849dc87097f7a53063d9ab466bfe28fe8f
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-06-14 18:46:51 +0530

    Middleware added

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,20 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 export default function authmiddleware(req: Request, res: Response, next: NextFunction): void {
-    const token = req.headers.authorization as string | undefined;
-
+    const token = req.headers.authorization; 
+    // Check if the token is provided in the Authorization header
     if (!token) {
         res.status(401).json({ message: "No token provided" });
         return;
     }
-
+    // Remove 'Bearer ' prefix if it exists
     try {
-        const payload = jwt.verify(token, "SecretPasword") as { id: string };
-        // @ts-ignore
-        req.id = payload.id;
-        next();
+        const payload = jwt.verify(token , JWT_SECRET);
+        if(payload){
+            //@ts-ignore
+            req.id = payload.id;
+        }
+        next(); // Call the next middleware or route handler
     } catch (e) {
         res.status(401).json({ message: "Invalid token" });
     }
+
 }
